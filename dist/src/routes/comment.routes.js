ac49c65df6f379f6dc2ebe3ad4df3fbba36e164d
Raqibreyaz <raquibreyaz111@gmail.com>
2024-09-10 20:35:53 +0530

    updated vercel.json

--- dist/src/routes/comment.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createComment, fetchComments, } from "../controllers/comment.controllers.js";
-import { verifyUser } from "../middlewares/verifyUser.middlewares.js";
-const Router = express.Router();
-Router.route("/create-comment/:id").post(verifyUser, createComment);
-Router.route("/get-comments/:id").get(fetchComments);
-export default Router;
